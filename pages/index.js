087eb1acce06f90a5d1cf7fd66182f5ae2f1a9ec
elvinciqueira <elvinciqueira@gmail.com>
2021-02-10 20:36:14 -0300

    feat: aula 04

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Footer from '../src/components/common/Footer'
 import Menu from '../src/components/common/Menu'
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        flex: '1',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
       <Menu />
+      <Footer />
     </div>
   )
 }
