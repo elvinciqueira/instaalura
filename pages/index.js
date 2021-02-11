c32dba136ab03abed8295d77bb681075bc02a373
elvinciqueira <elvinciqueira@zsystems.com.br>
2021-02-11 16:42:23 -0200

    feat: aula 08

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,78 @@
 import {Button} from '../src/components/common/Button'
 import Footer from '../src/components/common/Footer'
 import Menu from '../src/components/common/Menu'
+import {Box} from '../src/components/foundation/layout/Box'
+import {Grid} from '../src/components/foundation/layout/Grid'
 import Typography from '../src/components/foundation/Typography'
 
 export default function Home() {
   return (
-    <div
-      style={{
-        flex: '1',
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
+    <Box
+      flex={1}
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
     >
       <Menu />
 
-      <div>
-        <Typography
-          variant="title"
-          tag="h1"
-          color="tertiary.main"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Compartilhe momentos e conecte-se com amigos
-        </Typography>
-        <Typography
-          variant="paragraph1"
-          tag="p"
-          color="tertiary.light"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s.
-        </Typography>
+      <Grid.Container marginTop={{xs: '32px', md: '32px'}}>
+        <Grid.Row>
+          <Grid.Col
+            offset={1}
+            value={{xs: 12, md: 5}}
+            display="flex"
+            justifyContent="center"
+            alignItems="flex-start"
+            flexDirection="column"
+          >
+            <div>
+              <Typography
+                variant="title"
+                tag="h1"
+                color="tertiary.main"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Compartilhe momentos e conecte-se com amigos
+              </Typography>
+              <Typography
+                variant="paragraph1"
+                tag="p"
+                color="tertiary.light"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s.
+              </Typography>
 
-        <Button
-          variant="primary.main"
-          margin={{
-            xs: 'auto',
-            md: 'initial',
-          }}
-          display="block"
-        >
-          Cadastrar
-        </Button>
-      </div>
-
+              <Button
+                variant="primary.main"
+                margin={{
+                  xs: 'auto',
+                  md: 'initial',
+                }}
+                display="block"
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </Grid.Col>
+          <Grid.Col marginTop={{xs: '32px'}} value={{xs: 12, md: 6}}>
+            <img
+              style={{display: 'block', margin: 'auto'}}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
       <Footer />
-    </div>
+    </Box>
   )
 }
