2528df0121d07bfc5448833ff0051ee76d76ccdb
elvinciqueira <elvinciqueira@gmail.com>
2021-03-23 16:19:37 -0300

    módulo 4 aula 2

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,6 +2,8 @@
 
 describe('/pages/app/login', () => {
   it('should type input and redirect to /app/profile', () => {
+    cy.intercept('https://instalura-api.vercel.app/api/login').as('userLogin')
+
     cy.visit('/app/login')
 
     cy.get('#formCadastro input[name="usuario"]').type('elvinciqueira')
@@ -10,5 +12,13 @@ describe('/pages/app/login', () => {
     cy.get('#formCadastro button[type="submit"]').click()
 
     cy.url().should('include', '/app/profile')
+
+    cy.wait('@userLogin').then((intercept) => {
+      const {token} = intercept.response.body.data
+
+      cy.getCookie('APP_TOKEN')
+        .should('exist')
+        .should('have.property', 'value', token)
+    })
   })
 })
--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,4 +1,5 @@
 import HttpClient from '../../infra/HttpClient'
+import {setCookie, destroyCookie} from 'nookies'
 
 export const loginService = {
   async login({username, password}) {
@@ -9,9 +10,23 @@ export const loginService = {
         password,
       },
     }).then((response) => {
+      const {token} = response.data
+      const DAY_IN_SECONDS = 86400
+
+      setCookie(null, 'APP_TOKEN', token, {
+        path: '/',
+        maxAge: DAY_IN_SECONDS * 7,
+      })
+
       console.log(response)
 
-      return response
+      return {
+        token,
+      }
     })
+  },
+
+  logout() {
+    destroyCookie(null, 'APP_TOKEN')
   },
 }
