3964995467eee846b4a4747749a538147ae580d3
elvinciqueira <elvinciqueira@gmail.com>
2021-03-24 16:10:24 -0300

    Page Object

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,16 +1,19 @@
 /// <reference types="cypress" />
 
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject'
+
 describe('/pages/app/login', () => {
   it('should type input and redirect to /app/profile', () => {
+    //Pré Teste
     cy.intercept('https://instalura-api.vercel.app/api/login').as('userLogin')
 
-    cy.visit('/app/login')
+    //Cenário
+    const loginScreen = new LoginScreenPageObject(cy)
+    loginScreen
+      .fillLoginForm({user: 'elvinciqueira', password: 'senhasegura'})
+      .submitLoginForm()
 
-    cy.get('#formCadastro input[name="usuario"]').type('elvinciqueira')
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura')
-
-    cy.get('#formCadastro button[type="submit"]').click()
-
+    //Asserções
     cy.url().should('include', '/app/profile')
 
     cy.wait('@userLogin').then((intercept) => {
