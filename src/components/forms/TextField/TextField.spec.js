bc5df979f84cefd851167624abc3d6153ee5f68f
elvinciqueira <elvinciqueira@gmail.com>
2021-03-30 19:29:45 -0300

    módulo 4 aula 7

--- src/components/forms/TextField/TextField.spec.js
+++ src/components/forms/TextField/TextField.spec.js
@@ -17,4 +17,47 @@ describe('<TextField />', () => {
     const textField = screen.getByPlaceholderText(/nome/i)
     expect(textField).toMatchSnapshot()
   })
+
+  describe('when field is valid', () => {
+    describe('and user is typing', () => {
+      test('the value must be updated', () => {
+        const onChangeMock = jest.fn()
+        render(
+          <TextField
+            placeholder="Nome"
+            value=""
+            onChange={onChangeMock}
+            name="nome"
+            isTouched
+          />,
+        )
+
+        const inputNome = screen.getByPlaceholderText(/nome/i)
+        user.type(inputNome, 'devsoutinho@gmail.com')
+        expect(onChangeMock).toHaveBeenCalledTimes(21)
+      })
+    })
+  })
+
+  describe('when field is invalid', () => {
+    test('displays the respective error message', () => {
+      render(
+        <TextField
+          placeholder="Email"
+          value="devsoutinhogmail.com"
+          onChange={() => {}}
+          name="email"
+          isTouched
+          error="O campo email é obrigatório"
+        />,
+      )
+
+      const inputEmail = screen.getByPlaceholderText(/email/i)
+      expect(inputEmail).toHaveValue('devsoutinhogmail.com')
+      expect(screen.getByRole('alert')).toHaveTextContent(
+        'O campo email é obrigatório',
+      )
+      expect(inputEmail).toMatchSnapshot()
+    })
+  })
 })
--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -6,27 +6,30 @@ const useForm = ({initialState, onSubmit, validateSchema}) => {
   const [errors, setErrors] = useState({})
   const [touched, setTouchedFields] = useState({})
 
+  async function validateValues(currentValues) {
+    try {
+      await validateSchema(currentValues)
+      setErrors({})
+      setIsFormDisabled(false)
+    } catch (err) {
+      const formatedErrors = err.inner.reduce(
+        (errorObjectAcc, currentError) => {
+          const fieldName = currentError.path
+          const errorMessage = currentError.message
+          return {
+            ...errorObjectAcc,
+            [fieldName]: errorMessage,
+          }
+        },
+        {},
+      )
+      setErrors(formatedErrors)
+      setIsFormDisabled(true)
+    }
+  }
+
   useEffect(() => {
-    validateSchema(values)
-      .then(() => {
-        setIsFormDisabled(false)
-        setErrors({})
-      })
-      .catch((err) => {
-        const formatedErrors = err.inner.reduce(
-          (errorObjectAcc, currentError) => {
-            const fieldName = currentError.path
-            const errorMessage = currentError.message
-            return {
-              ...errorObjectAcc,
-              [fieldName]: errorMessage,
-            }
-          },
-          {},
-        )
-        setErrors(formatedErrors)
-        setIsFormDisabled(true)
-      })
+    validateValues(values)
   }, [values])
 
   const handleSubmit = (event) => {
