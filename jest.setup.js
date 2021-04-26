8edd91d325efe794c92a539de58bf8e2ec08e918
Elvin Ciqueira <elvinciqueira@gmail.com>
2021-04-26 13:50:22 -0300

    feat: component Link style

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
+import React from 'react'
+import WebsiteGlobalProvider from './src/components/wrappers/WebsitePage/provider'
+
 // Configs e Extensões do Jest
 import 'jest-styled-components' // Styled Components
 import 'jest-canvas-mock' // <Lottie /> (JS-DOM)
 import '@testing-library/jest-dom' // Traz expects decentes para lidar com styles e outras coisas do HTML
+import renderer from 'react-test-renderer'
+
+global.rendererCreateWithTheme = (Tree) =>
+  renderer.create(<WebsiteGlobalProvider theme={createTheme()}>{Tree}</WebsiteGlobalProvider>)
---
+++ src/components/common/Link/Link.spec.js
@@ -0,0 +1,20 @@
+import {Link} from './'
+import {render, screen} from '../../../infra/test/testUtils'
+import theme from '../../../theme'
+
+describe('<Link />', () => {
+  test('render correctly with default style', () => {
+    const tree = render(<Link href="/">link</Link>)
+    
+    const link = screen.getByRole('link', { name: /link/i})
+
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveStyleRule('color', 'inherit')
+    expect(link).toHaveStyleRule('text-decoration', 'none')
+    expect(link).toHaveStyleRule('opacity', '1')
+    expect(link).toHaveStyleRule('transition', `opacity ${theme.transition}`)
+    expect(link).toHaveStyleRule('opacity', '0.5', {
+      modifier: ':hover'
+    })
+  })
+})
