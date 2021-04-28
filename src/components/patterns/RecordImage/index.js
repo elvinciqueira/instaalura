f01fa5afaa8c9cf4c9c6d38a320a2e279dc2b0e3
elvinciqueira <elvinciqueira@gmail.com>
2021-04-28 15:08:10 -0300

    fix

--- src/components/patterns/RecordImage/index.js
+++ src/components/patterns/RecordImage/index.js
@@ -138,7 +138,6 @@ export default function RecordImage({propsDoModal, onClose}) {
 
           <Box
             padding="32px"
-            flex={1}
             width="100%"
             display="flex"
             flexDirection="column"
